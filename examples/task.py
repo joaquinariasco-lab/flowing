9f6d696431d766c8d21cf87c59c96eb4142cbaea
Joaquin Arias <joaquin.arias.co@gmail.com>
2026-01-31 23:42:09 -0300

    Delete examples/task.py

--- examples/task.py
+++
@@ -1,5 +0,0 @@
-class Task:
-    def __init__(self, description, price, criteria):
-        self.description = description
-        self.price = price
-        self.criteria = criteria  # function that validates the result
