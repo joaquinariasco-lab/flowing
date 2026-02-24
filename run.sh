8b4f22425ed7a0aa015db5ce6a25b72ffd06d496
Joaquin Arias <joaquin.arias.co@gmail.com>
2026-02-23 23:38:11 -0300

    Update run.sh
    Signed-off-by: Joaquin Arias <joaquin.arias.co@gmail.com>

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 source venv/bin/activate
 cd examples
 
-echo "Starting AgentA..."
+echo "Starting AgentA on port 5000..."
 python3 agent_server.py &
 
-echo "Starting AgentX..."
+echo "Starting AgentX on port 5001..."
 python3 my_agent_server.py &
+
+echo "✅ Agents running."
