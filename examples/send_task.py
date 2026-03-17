6238ff7a571784afb643ace32960fe17754a7175
Joaquin Arias <joaquin.arias.co@gmail.com>
2026-03-17 15:44:35 -0300

    Delete examples/send_task.py
    Signed-off-by: Joaquin Arias <joaquin.arias.co@gmail.com>

--- examples/send_task.py
+++
@@ -1,7 +0,0 @@
-import requests
-import sys
-
-task = sys.argv[1] if len(sys.argv) > 1 else "say hello"
-response = requests.post("http://127.0.0.1:5001/message", json={"task": task})
-print(response.json())
-
